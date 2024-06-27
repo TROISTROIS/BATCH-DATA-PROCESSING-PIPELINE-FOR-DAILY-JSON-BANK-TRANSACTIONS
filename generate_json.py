7ecc2fb3648e44b3e4e33ecd41275eae2e6b38db
TROIS <franciscangeno1@gmail.com>
2024-06-27 16:44:53 +0300

    Changed directory

--- generate_json.py
+++ generate_json.py
@@ -86,7 +86,7 @@ def generate_transactions(num_transactions, current_date):
 
 def write_to_json(data, filename):
     try:
-        with open("mock data.json", 'r') as file:
+        with open("/tmp/mock data.json", 'r') as file:
             existing_data = json.load(file)
     except FileNotFoundError:
         existing_data = []
